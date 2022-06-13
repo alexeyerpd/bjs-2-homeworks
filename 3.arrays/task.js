584544900a279c2ac240ecc8574fcd7ed46a92d9
alexeyerpd <alexeyerpd@gmail.com>
2022-06-13 21:21:52 +0300

    homework 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,21 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  return arr1.every((v, i) => arr2[i] === v);
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.reduce((acc, v) => {
+    if (v >= 0 && v % 3 === 0) {
+      acc.push(v * 10);
+    }
+    return acc;
+  },[]); 
 }
+
+// function advancedFilter(arr) {
+//   return arr
+//     .filter((v) => v >= 0 && v % 3 === 0)
+//     .map(v => v * 10); 
+// }
