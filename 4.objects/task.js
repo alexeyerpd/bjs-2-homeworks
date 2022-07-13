d0c7fa61887093c5d7338f661c306630636e4630
alexeyerpd <alexeyerpd@gmail.com>
2022-07-14 01:31:52 +0300

    homework 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,26 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  this.marks = (this.marks || []).concat(mark);
+}
+
+Student.prototype.getAverage = function() {
+  if (this.marks?.length) {
+    return this.marks.reduce((acc, v) => acc + v) / this.marks.length;
+  }
+  return 0;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
